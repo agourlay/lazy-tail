55a846a80fd9a6f9f1c6db87330af0ab3f48b9da
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2015-04-11 15:03:50 +0200

    clean up tests

--- src/test/scala/SourceSpec.scala
+++ src/test/scala/SourceSpec.scala
@@ -1,13 +1,16 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.Sink
 import akka.testkit.TestKit
-import lazyTail.{ LogLevel, LazyTail }
+import lazyTail.{ LazyLog, LogLevel, LazyTail }
 
 import org.scalatest._
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
+import org.scalatest.concurrent.{ Eventually, ScalaFutures }
+import org.scalatest.time.{ Millis, Span }
 import org.slf4j.LoggerFactory
 
-class SourceSpec(_system: ActorSystem) extends TestKit(_system) with WordSpecLike with Matchers with ScalaFutures with BeforeAndAfterAll {
+class SourceSpec(_system: ActorSystem) extends TestKit(_system) with WordSpecLike with Matchers with ScalaFutures with Eventually with BeforeAndAfterAll {
   def this() = this(ActorSystem("MySpec"))
 
   val logger = LoggerFactory.getLogger("test-logger")
@@ -23,9 +26,13 @@ class SourceSpec(_system: ActorSystem) extends TestKit(_system) with WordSpecLik
 
       val futureSource = LazyTail().source(LogLevel.INFO)
       whenReady(futureSource) { s ⇒
+        val process = s.runWith(Sink.head)
+
         logger.info("catch me if you can")
-        // FIXME
-        s.runForeach { log ⇒ log.formattedMessage should be("catch me if you can") }
+
+        whenReady(process) { log ⇒
+          log.formattedMessage should equal("catch me if you can")
+        }
       }
     }
   }
